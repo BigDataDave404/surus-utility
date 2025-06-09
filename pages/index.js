f73488c1a67313cc8a1f0f7a10bc47e4e475951e
David Hand <david@surusllc.com>
2025-06-09 13:17:03 -0400

    Attempting tailwind.css

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <h1 style={{ textAlign: "left" }}>Surus Utilities</h1>
-      <h2 style={{ textAlign: "left" }}>David Hand Test Sheet</h2>
       <ScriptRunner />
     </div>
   );
